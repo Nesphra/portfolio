0cb2a0978a765c8366634f5bdae2ef78be0859b7
Romeo Beaurain <beaurainr@Romeos-MacBook-Air.local>
2025-03-20 12:51:00 -0500

    updateD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to my porftolio</h1>
-      <p>Completely blank at the moment since I just made this. Check a few of my websites:</p>
-      <button>90 Days Challenge</button>
+      <p>Completely blank at the moment since I'm starting out. Check a few of my websites:</p>
+      <button><a href="https://90dayschallenge.vercel.app">90 Days Challenge</a></button>
+      {/* <button>90 Days Challenge</button> */}
     </div>
   );
 }
