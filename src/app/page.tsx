9069fe43f66aaa9d70d91b3db73a5141d549ca88
Romeo Beaurain <beaurainr@Romeos-MacBook-Air.local>
2025-03-20 12:38:53 -0500

    button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to my porftolio</h1>
       <p>Completely blank at the moment since I just made this. Check a few of my websites:</p>
-      <a href="https://90dayschallenge.vercel.app">90 Days Challenge</a>
+      <button>90 Days Challenge</button>
     </div>
   );
 }
